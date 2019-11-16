6b84c45be406c61c97482ab09e1e3d081dbe86ae
nonsodaniel <nonsodaniel07@gmail.com>
2019-11-16 15:12:17 +0100

    added build

---
+++ client/build/precache-manifest.a2606286e7b19a17bfe7144458e54747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc3cc3aeaa53512493a4d342e41c786d",
+    "url": "/static/media/logo.cc3cc3ae.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11316923c92c728b152e",
+    "url": "/static/js/main.11316923.chunk.js"
+  },
+  {
+    "revision": "afcb8e5cfdb342fc1a7e",
+    "url": "/static/js/2.afcb8e5c.chunk.js"
+  },
+  {
+    "revision": "11316923c92c728b152e",
+    "url": "/static/css/main.6ea06edd.chunk.css"
+  },
+  {
+    "revision": "63890f82415566b7214de69758b85db2",
+    "url": "/index.html"
+  }
+];
